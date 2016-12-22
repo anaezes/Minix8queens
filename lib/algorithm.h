bab73fe408263e8a767d943b5798d0d8405ddd06
up200700742 <up200700742@92026aab-b54b-4977-9cae-b409ada048fe>
2016-12-22 15:35:39 +0000

    Algorithm: code clean up
    git-svn-id: https://svn.fe.up.pt/repos/lcom1617-t1g11/proj@97 92026aab-b54b-4977-9cae-b409ada048fe

--- lib/algorithm.h
+++ lib/algorithm.h
@@ -4,15 +4,11 @@
 
 #define SIZE 8
 
-void putQueen(int matrix[SIZE][SIZE], int x, int y);
-
 int is_valid(int matrix[SIZE][SIZE], int x, int y);
 
 int valid_left_diag(int matrix[SIZE][SIZE], int x, int y);
 
 int valid_right_diag(int matrix[SIZE][SIZE], int x, int y);
-
-void removeQueen(int matrix[SIZE][SIZE], int x, int y);
 
 
 void printBoard(int matrix[SIZE][SIZE]);
--- src/algorithm.c
+++ src/algorithm.c
@@ -13,122 +13,111 @@ int valid_left_diag(int matrix[SIZE][SIZE], int x, int y);
 
 int valid_right_diag(int matrix[SIZE][SIZE], int x, int y);
 
-void putQueen(int matrix[SIZE][SIZE], int x, int y)
-{
-  matrix[y][x] = 1;
-}
-
 int is_valid(int matrix[SIZE][SIZE], int x, int y)
 {
-  int n_queens = 0;
-  int i, j;
+	int n_queens = 0;
+	int i, j;
 
-  //LINE
-  for(i = 0; i < SIZE; i++)
-  {
-    if(matrix[y][i] == 1)
-      n_queens++;
+	//LINE
+	for(i = 0; i < SIZE; i++)
+	{
+		if(matrix[y][i] == 1)
+			n_queens++;
 
-    if(n_queens > 1)
-      return 1;
-  }
+		if(n_queens > 1)
+			return 0;
+	}
 
 
-  //COLLUMN
-  n_queens = 0;
-  for(j = 0; j < SIZE; j++)
-    {
-      if(matrix[j][x] == 1)
-        n_queens++;
+	//COLLUMN
+	n_queens = 0;
+	for(j = 0; j < SIZE; j++)
+	{
+		if(matrix[j][x] == 1)
+			n_queens++;
 
-      if(n_queens > 1)
-        return 1;
-    }
+		if(n_queens > 1)
+			return 0;
+	}
 
-  //DIAGONAL
-  if(valid_left_diag(matrix, x, y) == 1)
-    return 1;
-  if(valid_right_diag(matrix, x, y) == 1)
-    return 1;
+	//DIAGONAL
+	if(!valid_left_diag(matrix, x, y))
+		return 0;
+	if(!valid_right_diag(matrix, x, y))
+		return 0;
 
-  return 0;
+	return 1;
 }
 
 int valid_left_diag(int matrix[SIZE][SIZE], int x, int y)
 {
-  int j, i;
-  int n_queens = 0;
-  if(x > y)
-    {
-      i = 0;
-      j = x-y;
-    }
-  else
-    {
-      i = y-x;
-      j = 0;
-    }
+	int j, i;
+	int n_queens = 0;
+	if(x > y)
+	{
+		i = 0;
+		j = x-y;
+	}
+	else
+	{
+		i = y-x;
+		j = 0;
+	}
 
-  while(i < SIZE && j < SIZE)
-    {
-      if(matrix[i][j] == 1)
-        n_queens++;
+	while(i < SIZE && j < SIZE)
+	{
+		if(matrix[i][j] == 1)
+			n_queens++;
 
-      if(n_queens > 1)
-        return 1;
+		if(n_queens > 1)
+			return 0;
 
-      i++;
-      j++;
-    }
+		i++;
+		j++;
+	}
 
-  return 0;
+	return 1;
 }
 
 int valid_right_diag(int matrix[SIZE][SIZE], int x, int y)
 {
-  int i, j;
-  int n_queens = 0;
-  if(x + y >= 5)
-    {
-      i = abs(x+y-(SIZE-1));
-      j = SIZE-1;
-    }
-  else
-    {
-      i = 0;
-      j = x+y;
-    }
+	int i, j;
+	int n_queens = 0;
+	if(x + y >= 5)
+	{
+		i = abs(x+y-(SIZE-1));
+		j = SIZE-1;
+	}
+	else
+	{
+		i = 0;
+		j = x+y;
+	}
 
-  while(i < SIZE && j >= 0)
-    {
-      if(matrix[i][j] == 1)
-        n_queens++;
+	while(i < SIZE && j >= 0)
+	{
+		if(matrix[i][j] == 1)
+			n_queens++;
 
-      if(n_queens > 1)
-        return 1;
+		if(n_queens > 1)
+			return 0;
 
-      i++;
-      j--;
-    }
+		i++;
+		j--;
+	}
 
 
-  return 0;
+	return 1;
 }
-
-void removeQueen(int matrix[SIZE][SIZE], int x, int y)
-{
-  matrix[y][x] = 0;
-}
-
 
 void printBoard(int matrix[SIZE][SIZE])
 {
-  int i, j;
-  for(i = 0; i < SIZE; i++)
-    {
-      for(j=0; j < SIZE; j++)
-        printf("%d ", matrix[i][j]);
-      printf("\n");
-    }
-  printf("\n\n");
+	int i, j;
+	for(i = 0; i < SIZE; i++)
+	{
+		for(j=0; j < SIZE; j++)
+			printf("%d ", matrix[i][j]);
+		printf("\n");
+	}
+	printf("\n\n");
 }
