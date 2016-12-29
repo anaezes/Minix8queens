dbd812bd75b9bc2c192a85dedeced9118d9cb578
up200700742 <up200700742@92026aab-b54b-4977-9cae-b409ada048fe>
2016-12-29 18:28:24 +0000

    RTC: changed coordinates
    git-svn-id: https://svn.fe.up.pt/repos/lcom1617-t1g11/proj@142 92026aab-b54b-4977-9cae-b409ada048fe

--- src/rtc.c
+++ src/rtc.c
@@ -117,61 +117,61 @@ void show_date(date_t* date)
 	int first_digit = date->hour / 10;
 	int second_digit = date->hour % 10;
 	pixmap_t px = get_pixmap_digit(first_digit);
-	vg_draw_pixmap(695, 220, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(695, 200, px.pixmap, px.width, px.height);
 	px = get_pixmap_digit(second_digit);
-	vg_draw_pixmap(740, 220, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(740, 200, px.pixmap, px.width, px.height);
 	px = get_pixmap_digit(PXMAP_DPOINTS);
-	vg_draw_pixmap(785, 235, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(785, 215, px.pixmap, px.width, px.height);
 
 	//minutes
 	first_digit = date->min / 10;
 	second_digit = date->min % 10;
 	px = get_pixmap_digit(first_digit);
-	vg_draw_pixmap(805, 220, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(805, 200, px.pixmap, px.width, px.height);
 	px = get_pixmap_digit(second_digit);
-	vg_draw_pixmap(850, 220, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(850, 200, px.pixmap, px.width, px.height);
 	px = get_pixmap_digit(PXMAP_DPOINTS);
-	vg_draw_pixmap(895, 235, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(895, 215, px.pixmap, px.width, px.height);
 
 	//seconds
 	first_digit = date->sec / 10;
 	second_digit = date->sec % 10;
 	px = get_pixmap_digit(first_digit);
-	vg_draw_pixmap(915, 220, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(915, 200, px.pixmap, px.width, px.height);
 	px = get_pixmap_digit(second_digit);
-	vg_draw_pixmap(960, 220, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(960, 200, px.pixmap, px.width, px.height);
 
 	//day
 	first_digit = date->day / 10;
 	second_digit = date->day % 10;
 	px = get_pixmap_small_digit(first_digit);
-	vg_draw_pixmap(810, 185, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(810, 165, px.pixmap, px.width, px.height);
 	px = get_pixmap_small_digit(second_digit);
-	vg_draw_pixmap(830, 185, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(830, 165, px.pixmap, px.width, px.height);
 	px = get_pixmap_small_digit(PXMAP_SPOINTS);
-	vg_draw_pixmap(855, 205, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(855, 185, px.pixmap, px.width, px.height);
 
 	//month
 	first_digit = date->month / 10;
 	second_digit = date->month % 10;
 	px = get_pixmap_small_digit(first_digit);
-	vg_draw_pixmap(865, 185, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(865, 165, px.pixmap, px.width, px.height);
 	px = get_pixmap_small_digit(second_digit);
-	vg_draw_pixmap(885, 185, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(885, 165, px.pixmap, px.width, px.height);
 	px = get_pixmap_small_digit(PXMAP_SPOINTS);
-	vg_draw_pixmap(905, 205, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(905, 185, px.pixmap, px.width, px.height);
 
 	//year
 	px = get_pixmap_small_digit(2);
-	vg_draw_pixmap(920, 185, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(920, 165, px.pixmap, px.width, px.height);
 	px = get_pixmap_small_digit(0);
-	vg_draw_pixmap(940, 185, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(940, 165, px.pixmap, px.width, px.height);
 
 	date->year = date->year % 100;
 	first_digit = date->year / 10;
 	second_digit = date->year % 10;
 	px = get_pixmap_small_digit(first_digit);
-	vg_draw_pixmap(960, 185, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(960, 165, px.pixmap, px.width, px.height);
 	px = get_pixmap_small_digit(second_digit);
-	vg_draw_pixmap(980, 185, px.pixmap, px.width, px.height);
+	vg_draw_pixmap(980, 165, px.pixmap, px.width, px.height);
 }
