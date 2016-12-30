3977a1c78befde6014d18f73ba9889627052d42a
up200700742 <up200700742@92026aab-b54b-4977-9cae-b409ada048fe>
2016-12-30 14:52:08 +0000

    Algorithm: Added documentation
    git-svn-id: https://svn.fe.up.pt/repos/lcom1617-t1g11/proj@151 92026aab-b54b-4977-9cae-b409ada048fe

--- src/algorithm.h
+++ src/algorithm.h
@@ -1,12 +1,39 @@
 #ifndef __ALGORITHM_H
 #define __ALGORITHM_H
 
+/**
+ * Board size
+ **/
 #define SIZE 8
 
+
+/**
+ * Verify if a given queen position is valid.
+ * @param game matrix
+ * @param queen x coordinate
+ * @param queen y coordinate
+ * @return 1 if is valid
+ **/
 int is_valid(int matrix[SIZE][SIZE], int x, int y);
 
+
+/**
+ * Verify the left diagonal of a given queen position.
+ * @param game matrix
+ * @param queen x coordinate
+ * @param queen y coordinate
+ * @return 1 if is valid
+ **/
 int valid_left_diag(int matrix[SIZE][SIZE], int x, int y);
 
+
+/**
+ * Verify the right diagonal of a given queen position.
+ * @param game matrix
+ * @param queen x coordinate
+ * @param queen y coordinate
+ * @return 1 if is valid
+ **/
 int valid_right_diag(int matrix[SIZE][SIZE], int x, int y);
 
 #endif
